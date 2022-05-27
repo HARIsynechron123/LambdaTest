7595d23c8314c7bea97769c4e4eaa159a9627b43
HARIsynechron123 <93141570+HARIsynechron123@users.noreply.github.com>
2022-05-27 14:50:39 +0530

    Update terraform_aws_lambda_function_test.go
    changed path of directory

--- terraform_aws_lambda_function_test.go
+++ terraform_aws_lambda_function_test.go
@@ -18,7 +18,7 @@ func TestTerraformAwsLambdaFunctionExample(t *testing.T) {
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "../LambdaFunc",
+		TerraformDir: "",
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created.
